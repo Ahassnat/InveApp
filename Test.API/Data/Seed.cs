caf815592e6b9bf90418eed78c70d1ef1f85a293
Abdallah <alhassnatabdallah@gmail.com>
2020-01-08 01:54:06 +0200

    5- Seed Class

---
+++ Test.API/Data/Seed.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+using Test.API.Models;
+
+namespace Test.API.Data
+{
+    public class Seed
+    {
+        private readonly UserManager<User> _userManager;
+        public Seed(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+
+        }
+
+        public void SeedUser()
+        {
+            if (_userManager.Users.Any())
+            {
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                foreach (var user in users)
+                {   
+                    _userManager.CreateAsync(user, "password").Wait();
+                }
+            }
+        }
+    }
+}
